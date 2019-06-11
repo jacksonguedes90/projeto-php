76d40db47c6863f551b85ca7fbd337c7483b685f
jacksonguedes90 <guedesj200@gmail.com>
2019-06-11 20:22:41 -0300

    criando minha table

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include 'conn.php';
+
+$sql ="SELECT * from BANDAS";
+$result = $conn->query($sql);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table>
+
+    <thead>
+    <tbody>
+    
+    </tbody>
+    </thead>
+    </table>
+</body>
+</html>
